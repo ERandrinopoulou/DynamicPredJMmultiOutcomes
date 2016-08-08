d897fbf9988e298fadd908f9719955eb90517a1f
Eleni-Rosalina Andrinopoulou <e.andrinopoulou@erasmusmc.nl>
2016-08-08 14:38:24 +0200

    Add files via upload

---
+++ Fit.R
@@ -0,0 +1,47 @@
+####
+# data = long formal data for the continuous longitudinal analysis
+# data2 = long formal data for the ordinal longitudinal analysis
+# data.id = data for the survival analysis
+####
+
+load("data.id1.Rdata")
+load("data1.Rdata")
+source("Functions.R")
+source("PrepareData.R")
+
+
+library(rjags)
+library(R2WinBUGS)
+
+################################################
+# Save model as txt
+source("ModelJAGS.R")
+filename <- file.path("JointModel1.txt")
+write.model(model, filename)
+
+################################################
+# run model in jags
+model.fit <- jags.model(file = "JointModel1.txt", data = Data, n.chains = con$n.chains, 
+                         n.adapt = con$n.adapt, quiet = con$quiet)
+
+
+update(model.fit, con$n.burnin)
+res <- coda.samples(model.fit, parms,  n.iter = con$n.iter - con$n.burnin, thin = con$n.thin)
+codaFit <- as.mcmc.list(res)
+
+################################################
+# results
+bss <- do.call(rbind,codaFit)
+colnames(bss)
+n.sims <- nrow(bss)
+sims.list <- vector("list", length(parms))
+names(sims.list) <- parms
+for (p in seq_along(parms)) {
+  ii <- grep(paste("^", parms[p], sep = ""), colnames(bss))
+  sims.list[[p]] <- bss[, ii]
+}
+
+save(codaFit, file = "results.RData")
+######### sims.list is a list with the results. Then you will need to obtain the mean/median/mode of each chain
+
+
